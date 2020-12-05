bbae98a1b0af0d4ccb592c992317c6df6597cc8d
perfree <perfree@126.com>
2020-12-05 11:52:22 +0800

    是的没错,又换成了thymeleaf

--- perfree-web/src/main/resources/public/js/login.js
+++ perfree-web/src/main/resources/public/js/login.js
@@ -1,12 +1,21 @@
+var element, layer;
+layui.use(['element', 'layer'], function () {
+    element = layui.element;
+    layer = layui.layer;
+    layer.config({
+        offset: '10%'
+    });
+});
+
 $(".p-login-btn").on('click', function () {
     let account = $("input[name=account]").val();
     let password = $("input[name=password]").val();
     if (account === undefined || account === '' || account.length < 1){
-        pMessage('error','请输入账号');
+        layer.msg("请输入账号", {icon: 2});
         return;
     }
     if (password === undefined || password === '' || password.length < 1){
-        pMessage('error','请输入密码');
+        layer.msg("请输入密码", {icon: 2});
         return;
     }
     $.ajax({
@@ -18,11 +27,11 @@ $(".p-login-btn").on('click', function () {
             if (result.code === 200) {
                 location.href='/';
             } else {
-                pMessage('error',result.msg);
+                layer.msg(result.msg, {icon: 2});
             }
         },
         error : function() {
-            pMessage('error', "网络错误,请稍候重试");
+            layer.msg("网络错误,请稍候重试", {icon: 2});
         }
     });
 });
