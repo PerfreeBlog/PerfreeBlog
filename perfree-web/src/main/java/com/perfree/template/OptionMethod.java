0d488d287e48f11cb9d2e59d5a377106fcd4838a
perfree <perfree@126.com>
2020-12-05 11:27:32 +0800

    是的没错,又换成了thymeleaf

--- perfree-web/src/main/java/com/perfree/template/OptionMethod.java
+++ perfree-web/src/main/java/com/perfree/template/OptionMethod.java
@@ -1,23 +1,13 @@
 package com.perfree.template;
 
-/*
 import com.perfree.common.SpringBeanUtils;
+import com.perfree.model.Option;
 import com.perfree.service.OptionService;
-import freemarker.template.TemplateMethodModelEx;
-import freemarker.template.TemplateModelException;
-import org.springframework.stereotype.Component;
 
-import java.util.List;
+public class OptionMethod {
+    private static final OptionService optionService = SpringBeanUtils.getBean(OptionService.class);
 
-public class OptionMethod implements TemplateMethodModelEx {
-    private final OptionService optionService = SpringBeanUtils.getBean(OptionService.class);
-
-    @Override
-    public Object exec(List list) throws TemplateModelException {
-        if (list.size() != 1) {
-            throw new TemplateModelException("Wrong arguments");
-        }
-        return optionService.getOptionByKey(list.get(0).toString());
+    public static Option exec(String key){
+        return optionService.getOptionByKey(key);
     }
 }
-*/
