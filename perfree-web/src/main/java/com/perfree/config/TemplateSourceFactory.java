0faf7fbca086ab7cac771559ae6a94c17c53027d
perfree <perfree@126.com>
2021-08-12 11:40:20 +0800

    TemplateSourceFactory 404

--- perfree-web/src/main/java/com/perfree/config/TemplateSourceFactory.java
+++ perfree-web/src/main/java/com/perfree/config/TemplateSourceFactory.java
@@ -5,6 +5,7 @@ import com.jfinal.template.source.FileSource;
 import com.jfinal.template.source.ISource;
 import com.jfinal.template.source.ISourceFactory;
 import com.perfree.common.Constants;
+import com.perfree.common.OptionCacheUtil;
 
 import java.io.File;
 
@@ -22,16 +23,34 @@ public class TemplateSourceFactory implements ISourceFactory {
             return new FileSource(Constants.RESOURCES_DIR, s1, s2);
         }
 
-        File pluginsFile = new File(Constants.PLUGINS_DIR + s1);
-        if (pluginsFile.exists()) {
-            return new FileSource(Constants.PLUGINS_DIR, s1, s2);
-
-        }
-
         File projectFile = new File(FileUtil.file("").getAbsolutePath() + s1);
         if (projectFile.exists()) {
             return new FileSource(FileUtil.file("").getAbsolutePath(), s1, s2);
         }
+       String paths = view("/404.html", "/404.html",
+               "/static/admin/pages/exception/404.html");
+        File notFoundFile = new File(Constants.RESOURCES_DIR + paths);
+        if (notFoundFile.exists()) {
+            return new FileSource(Constants.RESOURCES_DIR, paths, s2);
+        }
+        File notFoundProjectFile = new File(FileUtil.file("").getAbsolutePath() + paths);
+        if (notFoundProjectFile.exists()) {
+            return new FileSource(FileUtil.file("").getAbsolutePath(), paths, s2);
+        }
         return null;
+    }
+
+    public String view(String validPath, String themeViewPath, String adminViewPath) {
+        File file = new File(Constants.PROD_THEMES_PATH + Constants.SEPARATOR +  OptionCacheUtil.getValue(Constants.OPTION_WEB_THEME) + validPath);
+        File devFile = new File(Constants.DEV_THEMES_PATH + Constants.SEPARATOR +  OptionCacheUtil.getValue(Constants.OPTION_WEB_THEME) + validPath);
+        if (file.exists() || devFile.exists()) {
+            return view("static/themes/" +  OptionCacheUtil.getValue(Constants.OPTION_WEB_THEME) + themeViewPath);
+        } else {
+            return view(adminViewPath);
+        }
+    }
+
+    public String view(String viewPath) {
+        return viewPath;
     }
 }
