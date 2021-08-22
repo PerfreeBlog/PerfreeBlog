49d13bb6d6d2368def8f236b26dee338141f5209
perfree <perfree@126.com>
2021-08-22 16:21:47 +0800

    add: ApiController

---
+++ perfree-web/src/main/java/com/perfree/controller/BaseApiController.java
@@ -0,0 +1,12 @@
+package com.perfree.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Api controller基类
+ */
+@RestController
+@CrossOrigin
+public class BaseApiController {
+}
