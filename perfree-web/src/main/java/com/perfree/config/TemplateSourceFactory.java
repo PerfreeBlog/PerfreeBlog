1639e9c9e3c362c967a02b9a8f96eff163bd46fc
perfree <perfree@126.com>
2021-12-20 17:51:09 +0800

    修复: 插件不能定义主题内html路径问题

--- perfree-web/src/main/java/com/perfree/config/TemplateSourceFactory.java
+++ perfree-web/src/main/java/com/perfree/config/TemplateSourceFactory.java
@@ -16,6 +16,7 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.net.URL;
 import java.util.Map;
 
 /**
@@ -27,30 +28,76 @@ public class TemplateSourceFactory implements ISourceFactory {
 
     @Override
     public ISource getSource(String s, String s1, String s2) {
-        try{
+        URL resource = getClassLoader().getResource(buildClassPathFinalFileName(null, s1));
+        if (resource != null) {
             return new ClassPathSourceFactory().getSource(null, s1, s2);
-        }catch (Exception e) {
-            try{
-                HttpServletRequest request =((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-                RequestMappingHandlerMapping handlerMapping = SpringBeanUtils.getBean(RequestMappingHandlerMapping.class);
-                HandlerExecutionChain handlerChain = handlerMapping.getHandler(request);
-                if (handlerChain == null) {
-                    throw new IllegalArgumentException("File not found!");
-                }
-                HandlerMethod handler = (HandlerMethod) handlerChain.getHandler();
-                Object bean = handler.getBean();
-                ClassLoader classLoader = bean.getClass().getClassLoader();
-                Map<String, PluginInfo> allPlugin = PluginHolder.getAllPlugin();
-                for (Map.Entry<String, PluginInfo> entry : allPlugin.entrySet()) {
-                    if (entry.getValue().getPluginWrapper().getPluginClassLoader() == classLoader) {
-                        return new FileSourceFactory().getSource(Constants.PLUGINS_RESOURCES_DIR + File.separator + entry.getKey(), s1, s2);
-                    }
-                }
-
-                return new FileSourceFactory().getSource(Constants.RESOURCES_DIR, s1, s2);
-            } catch (Exception e2) {
-                return new FileSourceFactory().getSource(Constants.RESOURCES_DIR, s1, s2);
-            }
         }
+        String finalFileName = buildResourceFinalFileName(Constants.RESOURCES_DIR, s1);
+        File file = new File(finalFileName);
+        if (file.exists()) {
+            return new FileSourceFactory().getSource(Constants.RESOURCES_DIR, s1, s2);
+        }
+
+
+        try{
+            HttpServletRequest request =((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            RequestMappingHandlerMapping handlerMapping = SpringBeanUtils.getBean(RequestMappingHandlerMapping.class);
+            HandlerExecutionChain handlerChain = handlerMapping.getHandler(request);
+            if (handlerChain == null) {
+                throw new IllegalArgumentException("File not found!");
+            }
+            HandlerMethod handler = (HandlerMethod) handlerChain.getHandler();
+            Object bean = handler.getBean();
+            ClassLoader classLoader = bean.getClass().getClassLoader();
+            Map<String, PluginInfo> allPlugin = PluginHolder.getAllPlugin();
+            for (Map.Entry<String, PluginInfo> entry : allPlugin.entrySet()) {
+                if (entry.getValue().getPluginWrapper().getPluginClassLoader() == classLoader) {
+                    return new FileSourceFactory().getSource(Constants.PLUGINS_RESOURCES_DIR + File.separator + entry.getKey(), s1, s2);
+                }
+            }
+        }catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private String buildClassPathFinalFileName(String baseTemplatePath, String fileName) {
+        String finalFileName;
+        if (baseTemplatePath != null) {
+            char firstChar = fileName.charAt(0);
+            if (firstChar == '/' || firstChar == '\\') {
+                finalFileName = baseTemplatePath + fileName;
+            } else {
+                finalFileName = baseTemplatePath + "/" + fileName;
+            }
+        } else {
+            finalFileName = fileName;
+        }
+
+        if (finalFileName.charAt(0) == '/') {
+            finalFileName = finalFileName.substring(1);
+        }
+
+        return finalFileName;
+    }
+
+    private String buildResourceFinalFileName(String baseTemplatePath, String fileName) {
+        if (baseTemplatePath == null) {
+            return fileName;
+        }
+        char firstChar = fileName.charAt(0);
+        String finalFileName;
+        if (firstChar == '/' || firstChar == '\\') {
+            finalFileName = baseTemplatePath + fileName;
+        } else {
+            finalFileName = baseTemplatePath + File.separator + fileName;
+        }
+        return finalFileName;
+    }
+
+    private ClassLoader getClassLoader() {
+        ClassLoader ret = Thread.currentThread().getContextClassLoader();
+        return ret != null ? ret : getClass().getClassLoader();
     }
 }
