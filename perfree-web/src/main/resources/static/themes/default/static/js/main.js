6a34e614f351fd86cbddff0ad2ca031f55c7c945
perfree <perfree@126.com>
2022-01-20 16:51:32 +0800

    新的默认主题-增加返回顶部按钮(开发中)

--- perfree-web/src/main/resources/static/themes/default/static/js/main.js
+++ perfree-web/src/main/resources/static/themes/default/static/js/main.js
@@ -1,6 +1,9 @@
 new WOW().init();
 const colors = ["rgb(103, 204, 134)","rgb(52, 152, 219)","rgb(251, 155, 95)","rgb(244, 126, 96)","rgb(225, 91, 100)","rgb(132, 155, 135)","rgb(143, 197, 4)"];
-
+onWindowScroll();
+window.addEventListener('scroll', function(e){
+    onWindowScroll()
+})
 
 $('.d-category-item > span').each(function(){
     $(this).css("background-color", colors[RandomNum(0,6)]);
@@ -10,8 +13,18 @@ $('.d-tag-item').each(function(){
     $(this).css("background-color", colors[RandomNum(0,6)]);
 });
 
+$('.d-back-top-btn').on('click',function() {
+    $('body,html').animate({scrollTop: 0 },300);
+});
 
-
+function onWindowScroll(){
+    let scroll = $("html").scrollTop();
+    if (scroll > 50) {
+        $(".d-back-top-btn").addClass('show-back-up');
+    } else {
+        $(".d-back-top-btn").removeClass("show-back-up");
+    }
+}
 
 function RandomNum(Min,Max){
     var Range = Max - Min;
