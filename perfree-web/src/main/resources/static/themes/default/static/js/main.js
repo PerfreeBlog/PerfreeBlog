2d93b0e403ead8a4347e9053d621106755b82952
perfree <perfree@126.com>
2022-01-21 11:58:03 +0800

    新的默认主题-黑夜模式(响应式开发中)

--- perfree-web/src/main/resources/static/themes/default/static/js/main.js
+++ perfree-web/src/main/resources/static/themes/default/static/js/main.js
@@ -1,6 +1,8 @@
-new WOW().init();
 const colors = ["rgb(103, 204, 134)","rgb(52, 152, 219)","rgb(251, 155, 95)","rgb(244, 126, 96)","rgb(225, 91, 100)","rgb(132, 155, 135)","rgb(143, 197, 4)"];
+
+new WOW().init();
 onWindowScroll();
+
 window.addEventListener('scroll', function(e){
     onWindowScroll()
 })
@@ -21,6 +23,21 @@ $('.d-back-top-btn').on('click',function() {
     $('body,html').animate({scrollTop: 0 },300);
 });
 
+if (localStorage.getItem("dark")) {
+    $('body').addClass("d-night");
+}
+
+$('.d-web-color-switch-btn').on('click',function() {
+    let attr = $('body').attr('class');
+    if (attr && attr.indexOf('d-night') >= 0) {
+        $('body').removeClass('d-night');
+        localStorage.removeItem("dark");
+    } else {
+        $('body').addClass('d-night');
+        localStorage.setItem("dark", "true");
+    }
+});
+
 function onWindowScroll(){
     let scroll = $("html").scrollTop();
     if (scroll > 50) {
@@ -31,8 +48,7 @@ function onWindowScroll(){
 }
 
 function RandomNum(Min,Max){
-    var Range = Max - Min;
-    var Rand = Math.random();   
-    var num = Min + Math.round(Rand * Range);
-    return num;
+    let Range = Max - Min;
+    let Rand = Math.random();
+    return Min + Math.round(Rand * Range);
 }
