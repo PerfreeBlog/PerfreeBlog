7969a713e1908f24b663491dc2cf41a20b2898a8
perfree <perfree@126.com>
2022-08-15 14:01:16 +0800

    默认主题: 新增文章点赞支持,新增动态支持

--- perfree-web/src/main/java/com/perfree/controller/front/ArticleController.java
+++ perfree-web/src/main/java/com/perfree/controller/front/ArticleController.java
@@ -11,10 +11,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -56,6 +53,7 @@ public class ArticleController extends BaseController {
     }
 
     @GetMapping(value = {"/article/like", "/journal/like"})
+    @ResponseBody
     public ResponseBean like(@RequestParam("id") Long id){
         articleService.updateGreatCount(id);
         return ResponseBean.success("success", null);
