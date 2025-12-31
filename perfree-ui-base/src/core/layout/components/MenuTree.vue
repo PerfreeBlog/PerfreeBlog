02c5b24bba0a846f704b5cf77c79a6d77fed762f
Perfree <perfree@126.com>
2025-12-31 17:09:37 +0800

    菜单栏彩色图标

--- perfree-ui-base/src/core/layout/components/MenuTree.vue
+++ perfree-ui-base/src/core/layout/components/MenuTree.vue
@@ -3,7 +3,7 @@
     <template v-if="menu.children && menu.children.length > 0">
       <el-sub-menu :index="menu.id" class="menu-item">
         <template #title>
-          <el-icon v-if="menu.icon" class="menu-icon">
+          <el-icon v-if="menu.icon" class="menu-icon" :style="{ color: getIconColor(menu.id) }">
             <font-awesome-icon :icon="menu.icon" />
           </el-icon>
           <span>{{ menu.name }}</span>
@@ -13,7 +13,7 @@
     </template>
     <template v-else>
       <el-menu-item :index="menu.isFrame === 0 ? '/admin/frame/' + menu.id : menu.url" class="menu-item"  @click="clickMenu(menu)">
-        <el-icon v-if="menu.icon" class="menu-icon">
+        <el-icon v-if="menu.icon" class="menu-icon" :style="{ color: getIconColor(menu.id) }">
           <font-awesome-icon :icon="menu.icon" />
         </el-icon>
         <template #title>{{ menu.name }}</template>
@@ -27,6 +27,33 @@ import {toPage} from "@/core/utils/tabs.js";
 
 const router = useRouter();
 const props = defineProps(['menuList'])
+
+const iconColors = [
+  '#667eea',
+  '#f5576c',
+  '#4facfe',
+  '#43e97b',
+  '#fa709a',
+  '#a18cd1',
+  '#ff9a9e',
+  '#fcb69f',
+  '#66a6ff',
+  '#6f86d6',
+  '#f093fb',
+  '#48c6ef',
+]
+
+const colorCache = new Map()
+
+function getIconColor(menuId) {
+  if (colorCache.has(menuId)) {
+    return colorCache.get(menuId)
+  }
+  const hash = String(menuId).split('').reduce((acc, char) => acc + char.charCodeAt(0), 0)
+  const color = iconColors[hash % iconColors.length]
+  colorCache.set(menuId, color)
+  return color
+}
 
 function clickMenu(menu) {
   if (menu.isFrame === 0) {
@@ -43,10 +70,15 @@ function clickMenu(menu) {
 
 <style scoped>
 .menu-icon {
-  font-size: 14px!important;
+  font-size: 14px !important;
 }
+
 .menu-item {
   margin-top: 6px;
-  border-radius: 3px;
+  border-radius: 8px;
+}
+
+:deep(.el-sub-menu__title) {
+  border-radius: 8px;
 }
 </style>
